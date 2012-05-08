2bd4453c73d332e1207590758cd4a04f9ba04d58
Ryan Doom <ryan.doom@webascender.com>
2012-05-08 18:52:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileSendIt::Application.config.secret_token = 'd3468de2a7f4dd692147acfe70265708e4bb4fd5404a44f31882f0953e466c1dddc73fea42add6b569e2508e0b617b9040c2f245d8b02e3ef037555d2294ee2c'
